1b33657a1928b5d5ca27cd9d3fea19ba42d8ae08
Suyash Chavan <31501239+suyash-20@users.noreply.github.com>
2020-08-10 02:28:01 +0530

    Update d_ff.v

--- sequential_ckts/RTL/d_ff.v
+++ sequential_ckts/RTL/d_ff.v
@@ -1,1 +1,15 @@
+module dff(q, d, clk, set, rst);
+  
+input d, clk, set, rst;
+output reg q;
 
+always @(posedge clk)
+
+if (rst)
+q <= 0;
+else if (set)
+q <= 1;
+else
+q <= d;  //IF WE DONT MENTION THIS ELSE CONDITION< WE WILL ENCOUNTER A LATCH
+  
+endmodule
