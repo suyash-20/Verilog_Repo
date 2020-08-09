b103d786c41182e4a3b73bf47f5473fd8a431d46
Suyash Chavan <31501239+suyash-20@users.noreply.github.com>
2020-08-10 02:28:20 +0530

    Update d_ff_tb.v

--- sequential_ckts/Testbench/d_ff_tb.v
+++ sequential_ckts/Testbench/d_ff_tb.v
@@ -1,1 +1,40 @@
+module dff_tb;
 
+reg d, clk, set, rst;
+wire q;
+
+dff DU(q, d, clk, set, rst);
+
+always begin
+  #2clk=1'b1;
+  #2clk=1'b0;
+end
+  
+  
+initial begin
+  @(negedge clk)
+  rst = 1;
+  @(negedge clk)
+  rst = 0;
+  
+  #5;
+  @(posedge clk)d = 1;
+  @(posedge clk)d = 0;
+  @(posedge clk)d = 0;
+  @(posedge clk)d = 1;
+  
+  
+  
+#50 $finish;
+
+end
+initial 
+  $monitor("for input d:%b, output is:%b",d,q);
+
+initial 
+begin
+  $dumpfile("dff.vcd");
+$dumpvars();
+end
+
+endmodule
